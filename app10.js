72d370abbc04af623cdae96561135392fb861007
Sarthak Prajapati <sarthakprajapati@live.in>
2019-03-25 01:25:06 +0530

    Loop
    v-for

---
+++ app10.js
@@ -0,0 +1,14 @@
+new Vue({
+    el : '#vue-app',
+    data : {
+        departments : ['IT','CSE','ECE','EEE','ME','BE'],
+        students : [{
+            'name':'sarthak',
+            'age' : '21'
+        },
+        {
+            'name':'ishu',
+            'age' : '21'
+        }]
+    }
+});
